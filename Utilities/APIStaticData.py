4477088eb320419cee26270f834fe0074ec9140f
Sandhya Rani Shinde <shinde.sandhyarani94@gmail.com>
2024-03-10 01:01:58 +0530

    API comments

--- Utilities/APIStaticData.py
+++ Utilities/APIStaticData.py
@@ -1,8 +1,14 @@
+'''API static module'''
+#import os is an module,provides way to interact with operating system
 import os
 
+#EnvironmentReader is an Class for reading variable
 class EnvironmentReader:
+    #Define an empty constructor, init is contructor method and self is ref can named
     def __init__(self):
+        #Pass is an statement used with init method
         pass
-
+    #Defines method named get_specific_variable that takes a variable_name as a parameter. 
     def get_specific_variable(self, variable_name):
+        #os.environ.get(variable_name)method used to retrieve the value from specified environment variable.
         return os.environ.get(variable_name)
